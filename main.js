14af10f2bb121c666153668036116c80b5c32a82
kejerial <74850420+kejerial@users.noreply.github.com>
2024-01-01 18:55:41 -0800

    Fixes

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import * as THREE from 'three';
-import gsap from './node_modules/gsap';
+import gsap from 'gsap';
 import countries from './countries.json';
 
-
 const canvasContainer = document.querySelector('#canvasContainer');
+// Setting up for Three.js
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, canvasContainer.offsetWidth / canvasContainer.offsetHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer(
@@ -14,6 +14,7 @@ const renderer = new THREE.WebGLRenderer(
 renderer.setSize(canvasContainer.offsetWidth, canvasContainer.offsetHeight)
 renderer.setPixelRatio(devicePixelRatio);
 
+// Creating a globe mesh with sphere geometry and basic material
 const globe = new THREE.Mesh( 
   new THREE.SphereGeometry(7, 50, 50), 
   new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('img/01-3.jpg')}));
@@ -22,8 +23,9 @@ group.add(globe);
 
 scene.add(group);
 
-camera.position.z = 15;
+camera.position.z
 
+// Creating stars for the background
 const starGeometry = new THREE.BufferGeometry()
 const starMaterial = new THREE.PointsMaterial({
   color: 0xffffff
@@ -45,12 +47,14 @@ starGeometry.setAttribute(
 const stars = new THREE.Points(starGeometry, starMaterial)
 scene.add(stars)
 
+// Selecting elements for the popup display from HTML file
 const popUp = document.querySelector('#popUp');
 const popUpName = document.querySelector('#name');
 const popUpType = document.querySelector('#type');
 const popUpValue = document.querySelector('#value');
 const dropDown = document.querySelector('#stat-types');
 
+// Creating boxes for each country based on the JSON data
 countries.forEach((country) => {
   const box = new THREE.Mesh(
     new THREE.BoxGeometry(.2, .2, 2),
@@ -61,6 +65,7 @@ countries.forEach((country) => {
     })
   );
 
+  // Calculating the position of each box based on country coordinates
   const radius = globe.geometry.parameters.radius;
   const latitude = (country.Latitude / 180) * Math.PI
   const longitude = (country.Longitude / 180) * Math.PI
@@ -82,8 +87,10 @@ countries.forEach((country) => {
   group.add(box);
 });
 
+// Setting initial rotation of the globe
 globe.rotation.y = - Math.PI / 2;
 
+// Creating a raycaster for mouse interaction
 const raycaster = new THREE.Raycaster();
 const mouse = {
   x: undefined,
@@ -97,14 +104,15 @@ const boxes = group.children.filter((mesh) => {
   return mesh.geometry.type === 'BoxGeometry';
 });
 
+// Function to get the highest value from the countries data based on the selected statistic
 function getHighest() {
   let highestValue = 0;
   for (let i = 0; i < countries.length; i++) {
     let value = String(countries[i][dropDown.value]);
     if (value != "") { 
+      // Handles commas, dollar signs, and ampersands in the data
       let num = (String(value)).replace(/[,]/g, '');
       num = Number(num.replace(/[$%]/g, ''));
-
       if (num > highestValue) {
         highestValue = num;
       }
@@ -113,26 +121,28 @@ function getHighest() {
   return highestValue;
 }
 
+// Animation function to render the scene and handle interactions
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
 
+  // Updating the raycaster with the current mouse position
   raycaster.setFromCamera(mouse, camera);
 
   var highestValue = getHighest();
 
+  // Resetting opacity for all meshes
   group.children.forEach((mesh) => {
     mesh.material.opacity = 0.5;
   })
 
-  // Errors for when value is int, 
+  // Adjusting the scale of boxes based on the selected statistic
   boxes.forEach(currentElement => {
     let curr = currentElement.object[dropDown.value];
     if (curr != "") {
       let num = (String(curr)).replace(/,/g, '');
       num = Number(num.replace(/[$%]/g, ''));
       currentElement.scale.set(1, 1, Math.max(num/highestValue, 1/9));
-      //currentElement.scale.set(1, 1, .5);
     }
     else {
       currentElement.scale.set(1, 1, 0);
@@ -145,6 +155,7 @@ function animate() {
 
   const intersects = raycaster.intersectObjects(boxes);
   
+  // Display the popup if the mouse is hovering over a box with corresponding data
   for (let i = 0; i < intersects.length; i++) {
     const box = intersects[i].object;
     box.material.opacity = 1;
@@ -164,6 +175,7 @@ function animate() {
 }
 animate();
 
+// Event listeners for mouse interactions
 canvasContainer.addEventListener('mousedown', ({clientX, clientY}) => {
   mouse.down = true;
   mouse.x_0 = clientX;
@@ -177,6 +189,7 @@ addEventListener('mouseup', (event) => {
 addEventListener('mousemove', (event) => {
   event.preventDefault();
 
+  // Normalizing mouse coordinates
   mouse.x = (event.clientX / canvasContainer.clientWidth) * 2 - 1
   mouse.y = -((event.clientY - (innerHeight - canvasContainer.clientHeight)) / canvasContainer.clientHeight) * 2 + 1;
 
